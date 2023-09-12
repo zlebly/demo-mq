8777058c4c54f17704bbd08fdad54ac28ff17387
douwenjie <douwj12345@gmail.com>
2023-09-12 17:50:43 +0800

    Initial Commit

---
+++ publisher/src/test/java/cn/itcast/mq/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itcast.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
