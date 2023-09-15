42e4a5dfbc9c14685a29776776d0a501555db3fa
douwenjie <douwj12345@gmail.com>
2023-09-15 17:56:37 +0800

    增加test

--- consumer/src/test/java/cn/itcast/mq/ConsumerApplicationTests.java
+++ consumer/src/test/java/cn/itcast/mq/ConsumerApplicationTests.java
@@ -1,13 +1,36 @@
 package cn.itcast.mq;
 
+import com.rabbitmq.client.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootTest
 class ConsumerApplicationTests {
 
     @Test
-    void contextLoads() {
+    void consumer() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("121.5.143.96");
+        factory.setPort(5673);
+        factory.setUsername("itcast");
+        factory.setPassword("123321");
+        factory.setVirtualHost("/");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        String queueName = "simple.queue";
+        channel.queueDeclare(queueName, false, false, false, null);
+        // 订阅消息
+        channel.basicConsume(queueName, true, new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) {
+                // 处理消息
+                System.out.println("消费者：" + new String(body));
+            }
+        });
+        System.out.println("消费者等待接收消息...");
     }
-
 }
--- publisher/src/test/java/cn/itcast/mq/PublisherApplicationTests.java
+++ publisher/src/test/java/cn/itcast/mq/PublisherApplicationTests.java
@@ -1,13 +1,38 @@
 package cn.itcast.mq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 @SpringBootTest
 class PublisherApplicationTests {
-
     @Test
-    void contextLoads() {
+    void publishTest() throws IOException, TimeoutException {
+        // 1.建立连接
+        ConnectionFactory factory = new ConnectionFactory();
+        // 1.1 设置连接参数
+        factory.setHost("121.5.143.96");
+        factory.setUsername("itcast");
+        factory.setPassword("123321");
+        factory.setPort(5673);
+        factory.setVirtualHost("/");
+        // 1.2 建立连接
+        Connection connection = factory.newConnection();
+        // 2.创建通道
+        Channel channel = connection.createChannel();
+        // 3.创建队列
+        String queueName = "simple.queue";
+        channel.queueDeclare(queueName, false, false, false, null);
+        // 4.发送消息
+        String msg = "hello";
+        channel.basicPublish("", queueName, null, msg.getBytes());
+        // 5.关闭连接资源
+        channel.close();
+        connection.close();
     }
-
 }
